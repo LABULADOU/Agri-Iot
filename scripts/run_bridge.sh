c4ff3115f06785be1f332a620a8fc691b9421141
root <root@localhost.localdomain>
2026-06-03 15:01:33 +0000

    chore: 项目更名 Agri-iot → Agri-lot

--- scripts/run_bridge.sh
+++ scripts/run_bridge.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "1234" | sudo -S python3 /home/admino/Agri-iot/scripts/serial_bridge.py /dev/ttyUSB0
+echo "1234" | sudo -S python3 /home/admino/Agri-lot/scripts/serial_bridge.py /dev/ttyUSB0
